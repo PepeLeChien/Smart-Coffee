79277a7e7271fb9d3357f3d6c279677f36cb66be
PepeLeChien <fabrizio0943@gmail.com>
2022-09-22 13:15:54 -0500

    testing js & finishing first select

---
+++ src/firstSelect.js
@@ -0,0 +1,16 @@
+const order = {
+    orderPlace: undefined,
+    orderTable: undefined,
+    clientName: undefined,
+    phoneNumber: undefined,
+    drink: undefined,
+}
+
+function eatInside(){
+    order.orderPlace = 'inside';
+}
+
+function eatOutside(){
+    order.orderPlace = 'outside';
+    location.href = "../index.html";
+}
